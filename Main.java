c0a51198c1a045ea726a09b8e463f36cfdb418ce
VasilenkoEleonora <vasilenko_ete@ukr.net>
2015-01-27 14:26:52 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,51 +12,37 @@ public static void main(String[] args) {
 	boolean exit = false;
 	int SIZE = 5;
 	System.out.println("Welcome to th STUDENTs Database!");
-	while (!exit)
-	{
-		for( int i=0; i < SIZE; ++i)
-		{
+	while (!exit)	{
+		for( int i=0; i < SIZE; ++i) {
 			System.out.println("Enter information about " + (i+1) + " student");
-			
 			System.out.println("Name: ");  
-				name = scan.next();
+		  	  name = scan.next();
 			System.out.println("Surname: "); 
-				surname = scan.next();
-			
-			for (;;)
-			{	
-				try
-				{
-					System.out.println("Age: ");
+			  surname = scan.next();
+			for (;;) {	
+		        	try{
+			         	System.out.println("Age: ");
 					age = scan.nextInt();
 					break;
 				}
-				catch(InputMismatchException e)
-				{
+				catch(InputMismatchException e)	{
 					System.out.println("Enter a number");
 				}
 			}
-			
 			System.out.println("Faculty: "); 
-				faculty = scan.next();
-			
+			faculty = scan.next();
 			group1.add(new Student(name,surname,age,faculty));
 		}
 		group1.printInfo();
-		
 		Operations.sortingOperation(group1);
 		System.out.println("What do you want to do? enter new student(e)/ sort(s)/ exit(x)");
-		
 		String choice = scan.nextLine();
-		
 		if(choice.compareTo("e") == 0)
 			continue;
-		else if(choice.compareTo("s") == 0)
-		{
+		else if(choice.compareTo("s") == 0){
 			Operations.sortingOperation(group1);
 		}
-		else if(choice.compareTo("x") == 0)
-		{
+		else if(choice.compareTo("x") == 0){
 			exit = true;
 		}
 		else
